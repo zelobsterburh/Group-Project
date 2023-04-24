518b202a098dab68c740fd33801a9bdebc4ea62a
kev2575 <124090093+kev2575@users.noreply.github.com>
2023-04-24 17:27:15 -0400

    completed users file in routes/api

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -29,3 +29,48 @@ userRouter.post("/signup", async(req,res) => {
         res.status(500).json({error: err.message});
     }
 });
+//Login Route
+userRouter.post("/login", async(req,res) => {
+    try {
+        const {email, password} = req.body;
+        if(!email || !password) {
+            return res.status(400).json({msg: "Please enter all the fields"});
+        }
+        const user = await User.findOne({email});
+        if(!user) {
+            return res
+            .status(400)
+            .send({msg: "User with this email does not exist"});
+        }
+        const isMatch = await bcryptjs.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).send({ msg:"Incorrect Password"});
+        }
+        const token = jwt.sign({ id: user._id}, "passwordKey")
+        res.json({token, user: {id: user._id, username: user.username} });
+    } catch(err) {
+        res.status(500).json({error: err.message});
+    }
+});
+// check if token is valid
+userRouter.post("/tokenIsValid", async(req,res) => {
+    try {
+        const token = req.header("x-auth-token");
+        if(!token) return res.json(false);
+        const verified = jwt.verify(token, "passwordKey");
+        if(!verified) return res.json(false);
+        const user = await User.findById(verified.id);
+        if(!user) return res.json(false);
+        return res.json(true)
+    } catch(err) {
+        res.status(500).json({error: err.message});
+    }
+});
+// to get user credentials
+userRouter.get("/", auth, async(req, res) => {
+    const user = await User.findById(req.user);
+    res.json({
+        username: user.username,
+        id: user._id,
+    });
+});
